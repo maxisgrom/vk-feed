308b66df56ecbae730f72282bc16c1d501b74b52
maxisgrom <johnybady@gmail.com>
2016-09-19 02:34:52 +0300

    Remove protocol from index file. Added basic template. JS not working yet. Need fixes

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 new Vue = ({
     el: '#app',
-    data: {}
+    data: {
+        posts: [],
+        url: 'https://api.vk.com/method/wall.get?owner_id=-550910&offset=0&count=5&filter=all&v=5.53',
+        loading: false
+    },
+    methods: {
+        load: function () {
+            this.loading = true
+            this.$http.get(this.url).then(function (responce) {
+
+                var json = responce.data,
+                    posts = json.responce.items
+
+                this.posts = this.posts.concat(posts)
+                this.url = json.next_url
+                this.loading = false
+
+            }, function (error) {
+                console.log(error)
+                this.loading = false
+            })
+        },
+        created: function () {
+            this.load()
+        }
+    }
 })
